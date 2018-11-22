03f985e0f2159d46b19345bb658be2cc4bd89e32
Luca Paganelli <luca.paga@gmail.com>
2018-11-22 10:56:41 +0000

    duration logs

--- dataproc/02.dataproc-create-cluster.sh
+++ dataproc/02.dataproc-create-cluster.sh
@@ -2,4 +2,6 @@
 
 . 00.variables.sh
 
+./90.log-time.sh "STARTING CLUSTER '${DATAPROC_CLUSTER_NAME}' ..."
 gcloud dataproc clusters create ${DATAPROC_CLUSTER_NAME} --region ${DATAPROC_CLUSTER_REGION} --zone ${DATAPROC_CLUSTER_ZONE} --scopes storage-rw
+./90.log-time.sh "CLUSTER '${DATAPROC_CLUSTER_NAME}' STARTED!"
